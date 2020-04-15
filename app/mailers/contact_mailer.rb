0093666f4f35ec62cae30c66ea818aeb801e1aee
Juan Manzo <juanmanzo@umail.ucsb.edu>
2020-04-15 18:09:11 +0000

    Email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'manzojh@gmail.com'
+  default to: 'johnmeekjh@gmail.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
