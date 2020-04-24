719eaeb07819d1fc98499e54694a8c5f9e6b1d4c
Juan Manzo <juanmanzo@umail.ucsb.edu>
2020-04-24 07:46:10 +0000

    User Page changes

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -2,64 +2,70 @@ var cached = 0;
 $(document).on('turbolinks:load', function(){
   
   console.log(cached);
-    $('#my-img').on("load", function() {
-    var colorThief = new ColorThief();
-    var sourceImage = $('#my-img')[0];
+  $('#my-img').on("load", function() {
+      var colorThief = new ColorThief();
+      var sourceImage = $('#my-img')[0];
+     
+      // Get the dominant color of image
+      try{
+        console.log("called2");
+        cached = cached + 1;
+        var color = colorThief.getPalette(sourceImage, 3);
+        console.log(color);
+        // Use the new color as background for body
+        if(color[1][0] < 100 || color[1][1] < 100 || color[1][2] < 100){
+          $('.alb-flag').css('color', 'white');
+          $('#spotify-link').css('color', 'white');
+        }
+        if(color[0][0] < 100 || color[0][1] < 100 || color[0][2] < 100){
+        $('.color-flag').css('color', 'white');
+      }
+        $('#color-change').css('background-color', 'rgb(' + color[0] + ')');
+        $('#my-img').css('border-color', 'rgb('+ color[2] + ')');
+        $('.details-well').css('background-color', 'rgb('+ color[1] + ')');
+        $(document.body).css('background-color', 'rgb('+ color[2] + ')');
+          console.log(color[2]);
+        $('.des-bg').css({
+            'background-color': 'rgb('+ color[0] + ')',
+              'border': '5px outset '+ 'rgb(' + color[2] + ')'
+            });
+        }
+      catch(err){
+        console.log(err.message);
+      }
+    });
+    
+    if (cached > 0){
  
-  // Get the dominant color of image
-  try{
-    console.log("called2");
-    cached = cached + 1;
-    var color = colorThief.getPalette(sourceImage, 3);
-    console.log(color);
+    // Get the dominant color of image
+    try{
+      console.log("called3");
+      var colorThief = new ColorThief();
+      var sourceImage = $('#my-img')[0];
+     var color = colorThief.getPalette(sourceImage, 3);
     // Use the new color as background for body
     if(color[1][0] < 100 || color[1][1] < 100 || color[1][2] < 100){
       $('.alb-flag').css('color', 'white');
+      $('#spotify-link').css('color', 'white');
     }
     if(color[0][0] < 100 || color[0][1] < 100 || color[0][2] < 100){
-    $('.color-flag').css('color', 'white');
-  }
+      $('.color-flag').css('color', 'white');
+    }
+    
+    
     $('#color-change').css('background-color', 'rgb(' + color[0] + ')');
     $('#my-img').css('border-color', 'rgb('+ color[2] + ')');
     $('.details-well').css('background-color', 'rgb('+ color[1] + ')');
       $(document.body).css('background-color', 'rgb('+ color[2] + ')');
       console.log(color[2]);
-  
-    $('.des-bg').css('background-color', 'rgb('+ color[0] + ')');
+    $('.des-bg').css({
+          'background-color': 'rgb('+ color[0] + ')',
+            'border': '5px outset '+ color[1]
+          });
     }
-  catch(err){
-    console.log(err.message);
-  }
-    });
-    
-    if (cached > 0){
- 
-  // Get the dominant color of image
-  try{
-    console.log("called3");
-    var colorThief = new ColorThief();
-    var sourceImage = $('#my-img')[0];
-   var color = colorThief.getPalette(sourceImage, 3);
-  // Use the new color as background for body
-  if(color[1][0] < 100 || color[1][1] < 100 || color[1][2] < 100){
-    $('.alb-flag').css('color', 'white');
-  }
-  if(color[0][0] < 100 || color[0][1] < 100 || color[0][2] < 100){
-    $('.color-flag').css('color', 'white');
-  }
-  
-  
-  $('#color-change').css('background-color', 'rgb(' + color[0] + ')');
-  $('#my-img').css('border-color', 'rgb('+ color[2] + ')');
-  $('.details-well').css('background-color', 'rgb('+ color[1] + ')');
-    $(document.body).css('background-color', 'rgb('+ color[2] + ')');
-    console.log(color[2]);
-
-  $('.des-bg').css('background-color', 'rgb('+ color[0] + ')');
-  }
-  catch(err){
-    console.log(err.message);
-  }
+    catch(err){
+      console.log(err.message);
+    }
     
   }
 });
